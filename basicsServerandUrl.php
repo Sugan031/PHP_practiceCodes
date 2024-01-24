47aedda1de1816abfca8270e42e682feefd6e20e
Sugan031 <suganthanv031@gmail.com>
2024-01-24 16:18:54 +0530

    date 24/01

---
+++ basicsServerandUrl.php
@@ -0,0 +1,21 @@
+<?php
+
+$url = parse_url('https://www.w3resource.com/php-exercises/php-basic-exercises.php');
+
+foreach($url as $key => $value){
+    echo "$key => $value<br>";
+}
+
+echo "<br><br>";
+
+// Define a text string
+$text = 'PHP Tutorial';
+
+// Use a regular expression to replace the first letter of each word with a span element with red color
+$text = preg_replace('/(\b[a-z])/i', '<span style="color:red;">\1</span>', $text);
+
+// Display the modified text with styled first letters
+echo $text;
+
+
+?>
---
+++ filehandling.php
@@ -0,0 +1,22 @@
+<?php
+   	$myfile = fopen("sample.txt","r");
+//    echo fread($myfile,filesize("sample.txt"))."<br>";
+   echo filesize("sample.txt")."<br>";
+//    echo fgets($myfile);
+// echo fgetc($myfile);
+while(!feof($myfile)) {
+  echo fgets($myfile) . "<br>";
+}
+   	fclose($myfile);
+ 	//  $file = fopen("sample.txt","a");
+	//  $textLine ="CSS = Cascading Style Sheets\n
+	//  HTML = Hyper Text Markup Language\n
+	//  PHP = PHP Hypertext Preprocessor\n
+	//  SQL = Structured Query Language\n
+	//  SVG = Scalable Vector Graphics\n
+	//  XML = EXtensible Markup Language\n";
+	//  fwrite($file, $textLine);
+
+	//  fclose($file);
+
+?>
