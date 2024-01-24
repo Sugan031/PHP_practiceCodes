b3b3bcbd5b61a90e13ce5c8f390461e17e1fbf1b
Sugan031 <suganthanv031@gmail.com>
2024-01-24 18:03:18 +0530

    Added a code after commit check

--- Oops.php
+++ Oops.php
@@ -1,3 +1,35 @@
 <?php
-    
+    class Fruits{
+        public $name;
+        public $color;
+
+        function set_name($name){
+            $this->name = $name;
+        }
+
+    function get_name(){
+        return $this-> name;
+    }
+
+    function set_color($color){
+        $this->color = $color;
+    }
+
+    function get_color(){
+        return $this->color;
+    }
+}
+
+$fruit_one = new Fruits();
+$Fruit_Two = new Fruits();
+
+$fruit_one->set_name("Apple");
+$Fruit_Two->set_name("Banana");
+$fruit_one->set_color("red");
+$Fruit_Two->set_color("yellow");
+
+echo "Name ". $fruit_one->get_name()." Color {$fruit_one->get_color()}<br>";
+echo "Name ".$Fruit_Two->get_name()." Color {$Fruit_Two->get_color()}";
+
+
 ?>
