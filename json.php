5986f85b363de6edaf4d178626ad4ecbee36d343
Sugan031 <suganthanv031@gmail.com>
2024-02-28 18:49:22 +0530

    updated

--- json.php
+++ json.php
@@ -5,14 +5,23 @@
 // $myfile = fopen("sample.json", "r");
 // echo fread($myfile,filesize("sample.json"))."<br>";
 // fclose($myfile);
-$myobj = file_get_contents("sample.json");
-$myObjOne = json_decode($myobj, true);
+// $myobj = file_get_contents("sample.json");
+// $myObjOne = json_decode($myobj, true);
 
-foreach ($myObjOne as $key => $value) {
-    echo "{$key} => {$value}<br>";
+// foreach ($myObjOne as $key => $value) {
+//     echo "{$key} => {$value}<br>";
+// }
+
+// print_r($myObjOne);
+
+// echo $myObjOne['name'];
+
+$myfile = file_get_contents("sample.json");
+$myfile = json_decode($myfile);
+// echo $myfile;
+print_r($myfile);
+
+foreach($myfile as $value) {
+    echo "<li>". $value->name ." " . $value->age . " " . $value->city ."</li><br>";
 }
-
-print_r($myObjOne);
-
-echo $myObjOne['name'];
 ?>
